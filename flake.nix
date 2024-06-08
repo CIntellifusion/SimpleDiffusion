2362e23b2cd7b1197ebc95d6a1c940ab12f714ca
timechess <tyc221@ruc.edu.cn>
2024-06-08 19:30:00 +0800

    more flexible nix

--- flake.nix
+++ flake.nix
@@ -7,13 +7,41 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgsGcc.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgsDrv.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgsGcc, nixpkgsDrv, flake-utils }:
   let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
+      pkgsDrv = import nixpkgsDrv {
         inherit system;
+        config.allowUnfree = true;
+        config.cudaSupport = true;
+      };
+      pkgsGcc = import nixpkgsGcc {
+        inherit system;
+      };
+      driverOverlay = final: prev: {
+        cudaDrivers = pkgsDrv.linuxPackages.nvidia_x11.overrideAttrs (old:
+        let version = "550.78"; in
+        {
+          src = pkgsDrv.fetchurl {
+          url = "https://download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run";
+          sha256 = "sha256-NAcENFJ+ydV1SD5/EcoHjkZ+c/be/FQ2bs+9z+Sjv3M=";
+        };
+        }
+        );
+      };
+      gccOverlay = final: prev: {
+        portableGcc = pkgsGcc.gcc;
+      };
+      overlays = [
+        driverOverlay
+        gccOverlay
+      ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
         config.allowUnfree = true;
         config.cudaSupport = true;
       }; in
--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@
 
   buildInputs = (with pkgs; [ python3 poetry gcc gnumake libGL zlib glib cudatoolkit]);
 
-  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath ([pkgs.linuxPackages.nvidia_x11 pkgs.stdenv.cc.cc.lib] ++ buildInputs);
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath ([pkgs.cudaDrivers pkgs.stdenv.cc.cc.lib] ++ buildInputs);
 
   shellHook = ''
   poetry env use ${pkgs.python3}/bin/python
