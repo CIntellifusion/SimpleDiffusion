ce57d3221297f23047d9a34155497d0a0ac6fcf1
timechess <tyc221@ruc.edu.cn>
2024-06-08 18:39:07 +0800

    fix unfree licence

--- flake.nix
+++ flake.nix
@@ -7,11 +7,15 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in { devShells.default = import ./shell.nix { inherit pkgs system; }; });
+  let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        config.cudaSupport = true;
+      }; in
+    { devShells.${system}.default = import ./shell.nix { inherit pkgs; };};
 }
